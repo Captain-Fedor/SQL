bcaea467aa70f187035448b7511b463e2b94e423
Fedor Sumkin <runway067@gmail.com>
2023-11-05 17:18:56 +0300

    add primary key

--- Script-4.sql
+++ Script-4.sql
@@ -5,30 +5,30 @@
 
 CREATE TABLE IF NOT EXISTS Artists (
 	artist_id SERIAL PRIMARY KEY,
-	artist_name TEXT
+	artist_name VARCHAR UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Genres (
 	genre_id SERIAL PRIMARY KEY,
-	genre_name TEXT
+	genre_name VARCHAR UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Albums (
 	album_id SERIAL PRIMARY KEY,
-	album_name TEXT,
+	album_name VARCHAR UNIQUE,
 	album_year DATE
 );	
 	
 CREATE TABLE IF NOT EXISTS Songs (
 	song_id SERIAL PRIMARY KEY,
+	song_name VARCHAR UNIQUE,
+	song_duration INTEGER,
 	album_id INTEGER REFERENCES Albums(album_id)
-	song_name TEXT,
-	song_duration INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Volumes (
 	volume_id SERIAL PRIMARY KEY,
-	volume_name TEXT,
+	volume_name VARCHAR,
 	volume_year DATE
 );	
 	
@@ -40,16 +40,16 @@ CREATE TABLE IF NOT EXISTS ArtistsGenres (
 );	
 	
 CREATE TABLE IF NOT EXISTS AlbumsArtists (
-	id SERIAL PRIMARY KEY,
 	artist_id INTEGER NOT NULL REFERENCES Artists(Artist_id),
-	album_id INTEGER NOT NULL REFERENCES Albums(album_id)
+	album_id INTEGER NOT NULL REFERENCES Albums(album_id),
+	CONSTRAINT AA_key PRIMARY KEY (Artist_id, album_id)
 );
 
 	
 CREATE TABLE IF NOT EXISTS VolumeSongs (
-	id SERIAL PRIMARY KEY,
 	Volume_id INTEGER NOT NULL REFERENCES Volumes(Volume_id),
-	Song_id INTEGER NOT NULL REFERENCES Songs(Song_id)
+	Song_id INTEGER NOT NULL REFERENCES Songs(Song_id),
+	CONSTRAINT VS_key PRIMARY KEY (Volume_id, Song_id)
 );
 
 	
